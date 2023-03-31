46569876fea924d0f4bd3631a7ecb1f208144d6b
TessaViergever <112861180+TessaViergever@users.noreply.github.com>
2023-03-31 09:29:37 +0200

    nav footer/nav head/css changes
    add: main nav to footer (progressive enhancement)
    add: some wiki stuff
    change: css nav stuff

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,16 @@
+// click hamburger icon = toggle menu drop up/down
 const menuButton = document.querySelector('#hamburger-menu')
 const nav = document.querySelector('.main-navigation')
 
 menuButton.addEventListener('click', function() {
-    nav.classList.toggle('open')
-    menuButton.classList.toggle('open')
-})
+   nav.classList.toggle('open')
+   menuButton.classList.toggle('open')
+ })
+
+
+
+// JS schrijven om menu hamburger te laten werken als JS enabled is
+
+//  if JS = enabeled
+//      prevent default <a> in header niet doorlinken naar nav in footer + nav in footer verbergen
+// else ?
